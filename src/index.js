c5f16d25fcfe6e938011825817d50b86ac21c1d7
Varga Zsigmond <petrikdiak@gmail.com>
2022-09-28 13:41:46 +0200

    darkmode

--- src/index.js
+++ src/index.js
@@ -2,3 +2,19 @@
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import 'bootstrap/dist/css/bootstrap.css';
 import './style.css';
+
+document.getElementById('darkmode').addEventListener("click", katt);
+
+function katt()
+{
+    var gomb=document.getElementById('darkmode');
+    document.body.classList.toggle('dark-mode');
+    if (gomb.textContent=="Dark mode") {gomb.textContent = "Light mode";
+gomb.style.backgroundColor='wheat'
+gomb.style.color='rgb(51, 51, 51)'}
+    else {gomb.textContent = "Dark mode"
+    gomb.style.backgroundColor= 'rgb(51, 51, 51)'
+    gomb.style.color='wheat'}
+    ;
+    
+}
