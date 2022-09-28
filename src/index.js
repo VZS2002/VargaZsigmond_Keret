707d6d55fa59819deb7eef5726cb8e1169b76f35
Varga Zsigmond <petrikdiak@gmail.com>
2022-09-28 14:11:05 +0200

    Befejezett

--- src/index.js
+++ src/index.js
@@ -18,3 +18,22 @@ gomb.style.color='rgb(51, 51, 51)'}
     ;
     
 }
+
+document.getElementById('url').addEventListener('change', url);
+document.getElementById('szel').addEventListener('input', szel);
+document.getElementById('vas').addEventListener('input', border);
+document.getElementById('szin').addEventListener('input', border);
+function url()
+{
+    document.getElementById('kep').src = document.getElementById('url').value;
+}
+
+function szel(){
+document.getElementById('kep').width = document.getElementById('szel').value;
+}
+
+
+
+function border(){
+    document.getElementById('kep').style.border = document.getElementById('vas').value + "px solid " + document.getElementById('szin').value;
+}
